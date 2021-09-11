32421405ddabedd27e6cc197ace87c6d9a3bf4f4
marcosleonrs <marcosleonrs@outlook.com>
2021-09-10 21:01:36 -0500

    Valores negativos sensores

--- sistmr-web/src/main/java/ipn/cic/web/sistmr/delegate/MedidasBD.java
+++ sistmr-web/src/main/java/ipn/cic/web/sistmr/delegate/MedidasBD.java
@@ -6,7 +6,6 @@
  */
 package ipn.cic.web.sistmr.delegate;
 
-
 import ipn.cic.sistmr.exception.MedidasException;
 import ipn.cic.sistmr.exception.NoExistePacienteException;
 import ipn.cic.sistmr.exception.NoExisteValoresRefException;
@@ -45,32 +44,32 @@ import org.jboss.ejb3.annotation.SecurityDomain;
 @SecurityDomain("other")
 public class MedidasBD implements MedidasBDLocal {
 
-private static final Logger logger = Logger.getLogger(MedidasBDLocal.class.getName());
+    private static final Logger logger = Logger.getLogger(MedidasBDLocal.class.getName());
 
     @EJB
     private PacienteSBLocal pacienteSB;
 
     @EJB
     private MedidasSBLocal medidasSB;
-    
+
     @EJB
     private MedicoSBLocal medicoSB;
-    
+
     @EJB
     private PersonaSBLocal personaSB;
-    
+
     @EJB
     private UsuarioSBLocal usuarioSB;
-    
+
     @EJB
     private CorreoSBLocal correoSB;
-    
+
     @EJB
     private ValoresReferenciaSBLocal valoresSB;
-        
+
     @Resource(name = "java:jboss/mail/sisMCasaMail")
     private Session mailSesion;
-            
+
     private EntPaciente cargarPaciente(long idPaciente) throws NoExistePacienteException {
         EntPaciente paciente = pacienteSB.getPaciente(idPaciente);
         logger.log(Level.INFO, "\tPaciente {0} recuperado.", idPaciente);
@@ -86,7 +85,7 @@ private static final Logger logger = Logger.getLogger(MedidasBDLocal.class.getNa
         EntPersona persona;
         EntUsuario usuario;
         JsonObject respuesta;
-        
+
         try {
 
             paciente = cargarPaciente(med.getIdPaciente());
@@ -106,94 +105,120 @@ private static final Logger logger = Logger.getLogger(MedidasBDLocal.class.getNa
             medidas.setPreArtDiastolica(med.getPreArtDiastolica());
 
             medidas = medidasSB.guardaMedidas(medidas);
-            logger.log(Level.INFO, "Medidas guardadas.");
-
-            medico = medicoSB.getMedicoDePaciente(paciente); 
+            medico = medicoSB.getMedicoDePaciente(paciente);
             usuario = usuarioSB.getUsuarioDeMedico(medico);
-                             
-            logger.log(Level.INFO, "Correo medico: {0}",usuario.getEmail()); 
-            
             persona = personaSB.getPersonaDePaciente(paciente.getIdPaciente());
-            
-            String asunto = "Parametros fuera del rango";
-            String cuerpo = "Paciente: "+ persona.getPrimerApellido() 
-                                        + " " + persona.getSegundoApellido()
-                                        + " " + persona.getNombre();
-            
-            String reporteMedidas = revisarMedidas(medidas);
-            logger.log(Level.INFO, "reporte medidas {0}",reporteMedidas); 
-            if(reporteMedidas.isBlank()){
-                logger.log(Level.INFO, "Parametros dentro de los rangos");
-                logger.log(Level.INFO, "satOxg: {0}",medidas.getSaturacionOxigeno());
-                logger.log(Level.INFO, "temp: {0}",medidas.getTemperatura());
-                logger.log(Level.INFO, "fresp: {0}",medidas.getFrecRespiratoria());
-                logger.log(Level.INFO, "fcard: {0}",medidas.getFrecCardiaca());
-                logger.log(Level.INFO, "pSis: {0}",medidas.getPreArtSistolica());
-                logger.log(Level.INFO, "pDias: {0}",medidas.getPreArtDiastolica());
-            }else{ 
-                logger.log(Level.INFO, "Parametros fuera de los rangos");
-                correoSB.enviarCorreo(mailSesion,usuario.getEmail(),asunto,cuerpo+reporteMedidas);
+
+            String reporteMedidas = revisaMedidas(medidas);
+
+            if (reporteMedidas.isBlank()) {
+                logger.log(Level.INFO, "Medidas dentro de los rangos:");
+                logger.log(Level.INFO, "satOxg: {0}", medidas.getSaturacionOxigeno());
+                logger.log(Level.INFO, "temp: {0}", medidas.getTemperatura());
+                logger.log(Level.INFO, "fresp: {0}", medidas.getFrecRespiratoria());
+                logger.log(Level.INFO, "fcard: {0}", medidas.getFrecCardiaca());
+                logger.log(Level.INFO, "pSis: {0}", medidas.getPreArtSistolica());
+                logger.log(Level.INFO, "pDias: {0}", medidas.getPreArtDiastolica());
+            } else {
+                logger.log(Level.INFO, "Medidas fuera de los rangos");
+                String pac = "Paciente: " + persona.getPrimerApellido()
+                        + " " + persona.getSegundoApellido()
+                        + " " + persona.getNombre();
+                correoSB.enviarCorreo(mailSesion, usuario.getEmail(), "Notificacion Medidas ", pac + reporteMedidas);
+
             }
-            
-            respuesta = Json.createObjectBuilder()
-                    .add("Respuesta", "0")
-                    .add("Descripción", "Medidas almacenadas correctamente.")
-                    .build();
+
+            if (errorSensores(medidas) == 0) {
+                respuesta = Json.createObjectBuilder()
+                        .add("Respuesta", "0")
+                        .add("Descripción", "Medidas almacenadas correctamente.")
+                        .build();
+            } else {
+                respuesta = Json.createObjectBuilder()
+                        .add("Respuesta", "7")
+                        .add("Descripción", "Error sensores.")
+                        .build();
+            }
+
         } catch (NoExistePacienteException ex) {
             logger.log(Level.SEVERE, "Error, paciente no econtrado : {0}", ex.getMessage());
-
             respuesta = Json.createObjectBuilder()
                     .add("Respuesta", "1")
                     .add("Descripción", "No existe paciente.")
                     .build();
         } catch (MedidasException ex) {
             logger.log(Level.SEVERE, "Error al guardar las medidas : {0}", ex.getMessage());
-
             respuesta = Json.createObjectBuilder()
                     .add("Respuesta", "3")
-                    .add("Descripción", "Itente mas tarde.")
+                    .add("Descripción", "Intente mas tarde.")
                     .build();
-        }catch(Exception ex){
+        } catch (Exception ex) {
             logger.log(Level.SEVERE, "Error inesperado del sistema : {0}", ex.getMessage());
 
             respuesta = Json.createObjectBuilder()
                     .add("Respuesta", "4")
-                    .add("Descripción", "Error inesperado del sistema : "+ex.getMessage())
+                    .add("Descripción", "Error inesperado del sistema : " + ex.getMessage())
                     .build();
-        }        
+        }
         return respuesta;
     }
-   
-   public String revisarMedidas(EntMedidas medidas){
+
+    private int errorSensores(EntMedidas medidas) {
+        if (medidas.getSaturacionOxigeno() < 0 || medidas.getTemperatura() < 0
+                || medidas.getFrecRespiratoria() < 0 || medidas.getFrecCardiaca() < 0
+                || medidas.getPreArtSistolica() < 0 || medidas.getPreArtDiastolica() < 0) {
+            return 1;
+        }
+        return 0;
+    }
+
+    private String revisaMedidas(EntMedidas medidas) {
         try {
             EntValoresReferencia valoresRef;
             valoresRef = valoresSB.getValoresReferenciaId(new Short("1"));
-            String medidaStr="";
-            
-            if(medidas.getSaturacionOxigeno()<valoresRef.getSatOxigenoAlertMax() || medidas.getSaturacionOxigeno()>=valoresRef.getSatOxigenoNormalMax()){
-                medidaStr += " \nSaturacion de Oxigeno: "+Float.toString(medidas.getSaturacionOxigeno());
+            String reporte = "";
+
+            if (medidas.getSaturacionOxigeno() < 0) {
+                reporte += " \nError sensor: Saturacion oxigeno";
+            } else if (medidas.getSaturacionOxigeno() < valoresRef.getSatOxigenoAlertMax() || medidas.getSaturacionOxigeno() >= valoresRef.getSatOxigenoNormalMax()) {
+                reporte += " \nSaturacion de Oxigeno: " + Float.toString(medidas.getSaturacionOxigeno());
             }
-            if(medidas.getTemperatura()<valoresRef.getTemperaturaNormalMin() || medidas.getTemperatura()>=valoresRef.getTemperaturaAlertMin()){
-                medidaStr += " \nTemperatura: "+Float.toString(medidas.getTemperatura());
+
+            if (medidas.getTemperatura() < 0) {
+                reporte += " \nError sensor: Temperatura";
+            } else if (medidas.getTemperatura() < valoresRef.getTemperaturaNormalMin() || medidas.getTemperatura() >= valoresRef.getTemperaturaAlertMin()) {
+                reporte += " \nTemperatura: " + Float.toString(medidas.getTemperatura());
             }
-            if(medidas.getFrecRespiratoria()<valoresRef.getFrecRespiratoriaNormalMin() || medidas.getFrecRespiratoria()>=valoresRef.getFrecRespiratoriaAlertMin()){
-                medidaStr += " \nFrecuencia respiratoria: "+ Short.toString(medidas.getFrecRespiratoria());
+
+            if (medidas.getFrecRespiratoria() < 0) {
+                reporte += " \nError sensor: Frecuencia Respiratoria";
+            } else if (medidas.getFrecRespiratoria() < valoresRef.getFrecRespiratoriaNormalMin() || medidas.getFrecRespiratoria() >= valoresRef.getFrecRespiratoriaAlertMin()) {
+                reporte += " \nFrecuencia respiratoria: " + Short.toString(medidas.getFrecRespiratoria());
             }
-            if(medidas.getFrecCardiaca()<valoresRef.getFrecCardiacaNormalMin() || medidas.getFrecCardiaca()>=valoresRef.getFrecCardiacaAlertMin()){
-                medidaStr += " \nFrecuencia cardiaca: "+ Short.toString(medidas.getFrecCardiaca());
+
+            if (medidas.getFrecCardiaca() < 0) {
+                reporte += " \nError sensor: Frecuencia Cardiaca";
+            } else if (medidas.getFrecCardiaca() < valoresRef.getFrecCardiacaNormalMin() || medidas.getFrecCardiaca() >= valoresRef.getFrecCardiacaAlertMin()) {
+                reporte += " \nFrecuencia cardiaca: " + Short.toString(medidas.getFrecCardiaca());
             }
-            if(medidas.getPreArtSistolica()<valoresRef.getPreArtSistolicaNormalMin() || medidas.getPreArtSistolica()>=valoresRef.getPreArtSistolicaAlertMin()){
-                medidaStr += " \nPresion Arterial Sistolica: "+ Integer.toString(medidas.getPreArtSistolica());
+
+            if (medidas.getPreArtSistolica() < 0) {
+                reporte += " \nError sensor: Presion Arterial Sistolica";
+            } else if (medidas.getPreArtSistolica() < valoresRef.getPreArtSistolicaNormalMin() || medidas.getPreArtSistolica() >= valoresRef.getPreArtSistolicaAlertMin()) {
+                reporte += " \nPresion Arterial Sistolica: " + Integer.toString(medidas.getPreArtSistolica());
             }
-            if(medidas.getPreArtDiastolica()<valoresRef.getPreArtDiastolicaNormalMin() || medidas.getPreArtDiastolica()>=valoresRef.getPreArtDiastolicaAlertMin()){
-                medidaStr += " \nPresion Arterial Diastolica: "+Integer.toString(medidas.getPreArtDiastolica());
+
+            if (medidas.getPreArtDiastolica() < 0) {
+                reporte += " \nError sensor: Presion Arterial Diastolica";
+            } else if (medidas.getPreArtDiastolica() < valoresRef.getPreArtDiastolicaNormalMin() || medidas.getPreArtDiastolica() >= valoresRef.getPreArtDiastolicaAlertMin()) {
+                reporte += " \nPresion Arterial Diastolica: " + Integer.toString(medidas.getPreArtDiastolica());
             }
-             
-            return medidaStr;
+
+            return reporte;
         } catch (NoExisteValoresRefException ex) {
             logger.log(Level.INFO, "Error al obtener valores de referencia");
         }
-        return null;
+        return "";
     }
-    
+
 }
